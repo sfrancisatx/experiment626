413ba46ed8f389f8791a7688dca2ef9443fb2470
Scott Francis <scott.francis@gmail.com>
2025-04-16 17:07:19 -0500

    creating a static folder and page to test
    making sure that static html works as a vehicle to get in
    making sure that everything compiles
    etc.
    Note if you just go to localhost:5111 it will take you to a react app that has some debug functionality that was provided by colyseus

--- experiment626-server/src/app.config.ts
+++ experiment626-server/src/app.config.ts
@@ -1,6 +1,11 @@
 import config from "@colyseus/tools";
 import { monitor } from "@colyseus/monitor";
 import { playground } from "@colyseus/playground";
+import { auth } from "@colyseus/auth";
+import path from 'path';
+//import serveIndex from 'serve-index';
+import express from 'express';
+
 
 /**
  * Import your Room files
@@ -40,6 +45,14 @@ export default config({
          * Read more: https://docs.colyseus.io/tools/monitor/#restrict-access-to-the-panel-using-a-password
          */
         app.use("/monitor", monitor());
+
+        // this was in the sample app but i honestly don't know what it does :)
+       //  app.use('/', serveIndex(path.join(__dirname, "static"),{'icons': true}));
+
+       /* SLF
+        * this next command allows the app to use the static subdirectory (and therefore to "See" html pages in that directory)
+        */
+        app.use('/', express.static(path.join(__dirname, "static")));
     },
 
 
