68846eb36fcac7560726b2c69b521fa4081a8a6f
Hollo-There <hmf.atx@gmail.com>
2026-03-17 10:33:03 -0500

    Hello

--- experiment626-client/src/main.ts
+++ experiment626-client/src/main.ts
@@ -126,7 +126,7 @@ if (!window.location.hash || window.location.hash === "#lobby" || window.locatio
         const user = await ensureAuthenticated();
         const idToken = await getIdToken();
         const roomId = window.location.hash.replace("#game-", "");
-        const empireName = prompt("What's up with your face?") || user.displayName || "Anonymous";
+        const empireName = prompt("Bla Bla Bla") || user.displayName || "Anonymous";
         let pixiInitialized = false;
         client.joinById<GalaxyState>(roomId, { empireName, idToken }).then(async (room: Room<GalaxyState>) => {
         console.log("✅ Joined room:", room.roomId);
