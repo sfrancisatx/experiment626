4d50c5bad3db88d14d49059a968425fab8fb1f8b
Hollo-There <hmf.atx@gmail.com>
2026-03-19 17:18:32 -0500

    Log statements, debugging fleet movement beng so fast

--- experiment626-server/src/rooms/Galaxy.ts
+++ experiment626-server/src/rooms/Galaxy.ts
@@ -596,6 +596,7 @@ export class Galaxy extends Room<GalaxyState> {
             console.error(`\nEmpire of Fleet not found\nOwner ID: ${owner}\n Location: Galaxy.fleetEndTimeCalculator()`);
             return clockTime + distance;
         }
+        console.log("Fleet End Time Calculator:", clockTime + distance/gridUnitsPerLightYear * (hoursPerTurn * 60 * 60 * 1000) / ((fleetEmpire.state.speed + 9) / 10)+ "\nDist: " + distance + "\nSpeed: " + fleetEmpire.state.speed);
         return clockTime + distance/gridUnitsPerLightYear * (hoursPerTurn * 60 * 60 * 1000) / ((fleetEmpire.state.speed + 9) / 10);
     }
     createFleet(sourceStarId: string, destinationStarId: string, ships: number, clientId: string) {
