17e654a6736881595f240807fc8d6ed6a413eea0
Hollo-There <hmf.atx@gmail.com>
2026-06-01 19:28:26 -0500

    Odd single time events, where the bad actor playerViewStates run twice in a row, likely a matter of chance, but increased counter to simply wait for at least 5 to come in before acknowledging any others.

--- experiment626-client/src/main.ts
+++ experiment626-client/src/main.ts
@@ -236,7 +236,7 @@ if (!window.location.hash || window.location.hash === "#lobby" || window.locatio
                 // Handle initial UI decision (first time receiving playerViewState)
                 if (!gameInitialized) {
                     console.log("PlayerViewState while game not initialized:", playerViewState);
-                    if (playerViewStateCount > 0) {
+                    if (playerViewStateCount > 5) {
                         if (isGameUninitialized) {
                             // Game not initialized, show startup UI with button
                             showStartupUI();
