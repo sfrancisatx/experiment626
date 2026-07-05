dbd097e5c9260f3e6c59a9d8b7ed52d97e3ab7e9
Hollo-There <hmf.atx@gmail.com>
2026-07-05 12:43:18 -0500

    Drawing border around hit area to see why there is an inconsistency between green space, ownership indicator and hit box despite same radius.

--- experiment626-client/src/main.ts
+++ experiment626-client/src/main.ts
@@ -739,6 +739,9 @@ function renderPlayerViewState(galaxyState: GalaxyState, viewState: PlayerViewSt
         sprite.interactive = true;
         sprite.cursor = "pointer";
         sprite.hitArea = new PIXI.Circle(sprite.x, sprite.y, 7);
+        const hitAreaBorder = new PIXI.Graphics()
+        hitAreaBorder.circle(sprite.x, sprite.y, 7).stroke({ color: 0xFFFFFF, width: 1 });
+        stage.addChild(hitAreaBorder);
 
         sprite.starData = star;
 
